8834dcf71fe2539ad1d3402ff98443b0e5ccc0db
Matheus Oliveira <matheus@MacBook-Pro-de-Matheus.local>
2014-07-06 21:48:24 -0300

    changed format invalid

--- spec/dummy/app/models/test_date.rb
+++ spec/dummy/app/models/test_date.rb
@@ -1,4 +1,4 @@
 class TestDate < ActiveRecord::Base
   stringify :start_at, format: "%d/%m/%Y"
-  stringify :end_at, format: "%d/%m/%Y %H:%I:%S"
+  stringify :end_at, format: "%d/%m/%Y %H:%M:%S"
 end
