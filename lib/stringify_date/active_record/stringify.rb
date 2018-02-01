29318640c69dda93e237aa31a63ccce7d91ed9b9
Matheus Oliveira <matheus.vetor@gmail.com>
2018-02-01 12:39:50 -0200

    Cleaner code and parse date by format

--- lib/stringify_date/active_record/stringify.rb
+++ lib/stringify_date/active_record/stringify.rb
@@ -7,43 +7,39 @@ module StringifyDate
     module Stringify
       extend ActiveSupport::Concern
 
+      def parse_value(name, value, format)
+        return nil unless value.present?
+
+        begin
+          Time.strptime(value, format)
+        rescue ArgumentError
+          instance_variable_set("@#{name}_invalid", true)
+          nil
+        end
+      end
+
       module ClassMethods
         def stringify(field, *args)
           column_name = field.to_s
-
-          options = args.extract_options!
-
-          format = options[:format] || '%Y-%m-%d'
-
-          name = [column_name, 'string'].join('_')
+          options     = args.extract_options!
+          format      = options[:format] || '%Y-%m-%d'
+          name        = [column_name, 'string'].join('_')
 
           define_method name do
             date = send(column_name)
-            date.strftime(format) unless date.nil?
+            date.strftime(format) if date.present?
           end
 
           define_method "#{name}=" do |value|
-            self.send(
-              "#{column_name}=",
-                if value.present?
-                  begin
-                    Time.parse(value)
-                  rescue ArgumentError
-                    instance_variable_set("@#{name}_invalid", true)
-                    nil
-                  end
-                else
-                  nil
-                end
-            )
+            parsed_value = parse_value(name, value, format)
+            send("#{column_name}=", parsed_value)
           end
 
           define_method "validate_#{name}" do
             errors.add(name.to_sym, I18n.t('errors.invalid')) if instance_variable_get("@#{name}_invalid")
           end
 
-          self.send(:validate, :"validate_#{name}")
-
+          send(:validate, :"validate_#{name}")
         end
       end
     end
